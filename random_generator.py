966a3ed584484f023cfe203c7b63e5edbdaab2fd
Sentinel18 <rikerik7@gmail.com>
2021-11-12 12:20:23 +0100

    9.óra

--- random_generator.py
+++ random_generator.py
@@ -1,3 +1,13 @@
 import random
 
 print(random.randint(0, 400))
+print(random.random())
+
+a = random.randint(-100,100)
+print(a)
+if a >0:
+    print("A szám pozitív")
+elif a<0:
+    print("A szám negatív")
+else:
+    print("A szám nulla")
