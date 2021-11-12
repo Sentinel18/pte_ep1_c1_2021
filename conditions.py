16815d7f5a86ccee0b724b57071f5ce417c110b1
Sentinel18 <rikerik7@gmail.com>
2021-11-12 10:46:37 +0100

    9.óra

---
+++ conditions.py
@@ -0,0 +1,17 @@
+number = 35
+if number > 100:
+    print("A szám nagyobb, mint 100.")
+else:
+    print("A szám nem nagyobb, mint 100.")
+
+if number % 2 == 0:
+    print("A szám páros")
+else:
+    print("A szám nem páros")
+
+number1= 3
+number2 = 9
+if number1 % number2 == 0:
+    print("Az egyik szám osztható a másikkal.")
+else:
+    print("A számok nem oszthatóak.")
