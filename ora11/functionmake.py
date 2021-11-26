0395764e483041d3c669c90e8348e96304572c0e
Bakó Erik <rikerik7@gmail.com>
2021-11-26 12:31:08 +0100

    11.óra

---
+++ ora11/functionmake.py
@@ -0,0 +1,21 @@
+import random
+"""
+def maximum_kereset(lista: list[float]) -> float:
+    max = lista[0]
+    for number in lista:
+        if max< number:
+            max = number
+    return max
+
+def kiiratas(szam: float) -> None:
+    print(szam)
+numbers = []
+for i in range(20):
+    numbers.append(random.randint(1,100))
+print(numbers)
+
+maximum = print(maximum_kereset(numbers))
+print(maximum)
+print(kiiratas(maximum))
+"""
+
