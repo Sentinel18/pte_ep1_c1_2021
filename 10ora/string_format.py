78017f99a9ba4de3bc4cb625ec837e7763d9e94e
Bakó Erik <rikerik7@gmail.com>
2021-11-26 10:10:46 +0100

    10.óra

--- 10ora/string_format.py
+++ 10ora/string_format.py
@@ -12,3 +12,17 @@ print("A number változó értéke: {}. Ha megszorzom kettővel, akkor {}-t kapo
 print(f"A number változó értéke: {number:<3}. Ha megszorzom kettővel, akkor {number2:<3}-t kapok.")
 print("A number változó értéke: {:^3}. Ha megszorzom kettővel, akkor {:^4}-t kapok.".format(number,
                                                                                       number2))
+
+#számformátumok
+number= 13
+print("A number változó bináris értéke: {number:b.")
+print("A number változó oktális értéke: {:0}".format(number))
+print(f"A number változó decimális értéke: {number:d}.")
+print("A number változó hexadecimális értéke: {:x} és {:x}".format(number) )
+
+
+
+
+
+#valós számok
+number = 100.1232
